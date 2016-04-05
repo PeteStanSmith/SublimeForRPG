620ce73c121321cdaac9a2b879ac9e02b0996d11
PeteStanSmith <peter.smith@jhc.co.uk>
2016-04-05 16:57:09 +0100

    Added files via upload

--- Systemitools/Systemitools/checkout.py
+++ Systemitools/Systemitools/checkout.py
@@ -10,11 +10,14 @@ class checkoutFromTracey(sublime_plugin.TextCommand):
     def run(self, edit):
         global logger
         global ir
+        self.view.set_status('task', 'Checking out source code')
         logger = Logger('log')
-        logger.log("Retrieval operation initiated")
+        logger.log("Retrieval operation initiated.")
         self.set_variables()
 
     def set_variables(self):
+        global count
+        count = 0
         global ir
         ir = ''
         self.view.window() \
@@ -50,7 +53,7 @@ class checkoutFromTracey(sublime_plugin.TextCommand):
             else:
                 library = 'F63FIGARO'
                 logger.log('User did not enter a library, ' +
-                           'defaulted to ' + library)
+                           'defaulted to ' + library + '.')
 
             self.view.window() \
                 .show_input_panel("Enter the file extension: ",
@@ -82,8 +85,11 @@ class checkoutFromTracey(sublime_plugin.TextCommand):
 
         fileName = 'C:\\jhc\\src\\RPG\\CR\\' + program + '.' + \
                    fileExtension
-
-        retrievalTarget = Utils.getRetrievalTarget(fileExtension)
+        try:
+            retrievalTarget = Utils.getRetrievalTarget(fileExtension)
+        except KeyError:
+            self.view.erase_status('task')
+            raise
 
         getOperation = ('RETR ' + retrievalTarget + '.' + program)
         logger.log(getOperation)
@@ -98,36 +104,74 @@ class checkoutFromTracey(sublime_plugin.TextCommand):
             try:
                 ftp.retrlines(getOperation, self.writeFile)
                 logger.log('Source code for ' + library + '/' +
-                           program + ' successfully retrieved')
+                           program + ' successfully retrieved.')
                 os.startfile(fileName)
                 file.close()
+                ftp.quit()
             except:
-                logger.log('Could not find source file')
+                logger.log('Could not find source file or ' +
+                           'file could not be parsed.')
+                ftp.quit()
                 file.close()
-                os.remove(fileName)
         except:
-            logger.log('Library does not exist')
+            logger.log('Library does not exist.')
+            ftp.quit()
             file.close()
             os.remove(fileName)
 
-        ftp.quit()
-
         logger.close()
+        self.view.erase_status('task')
         return
 
     def exit(self):
-        logger.log('User connection reset by peer')
+        logger.log('User connection reset by peer.')
         logger.close()
 
+    # Should extract this to a file writer when I get the chance.
+    # Should also pull out what chracter has been replaced
     def writeFile(self, line):
+        global count
+        count = count + 1
         try:
             file.write(line + "\n")
         except UnicodeEncodeError:
-            line = line.replace('\x82', '\x20')
-            line = line.replace('\x80', '\x20')
-            line = line.replace('\xA6', '\x20')
-            line = line.replace('\x9A', '\x20')
-            line = line.replace('\xA2', '\x20')
-            line = line.replace('\xA3', '\x20')
+            # This section will replace the none acsii
+            # characters with a blank space.
+            try:
+                # see-change header character
+                line = line.replace('\x80', '\x20')
+                #  ¦ Alt Gr pipe
+                line = line.replace('\xA6', '\x20')
+                # ¬ not sign
+                line = line.replace('\xAC', '\x20')
 
-            file.write(line + "\n")
+                # white hex colour
+                line = line.replace('\x82', '\x20')
+                # blue hex colour
+                line = line.replace('\x9A', '\x20')
+                # Purple hex colour
+                line = line.replace('\x98', '\x20')
+                # Red hex colour
+                line = line.replace('\x88', '\x20')
+                # Yellow hex colour
+                line = line.replace('\x96', '\x20')
+
+                # [ open square bracket - replaced with ^.
+                line = line.replace('\xA3', '\x5E')
+
+                file.write(line + "\n")
+
+            except UnicodeEncodeError:
+                # This section is for those characters
+                # that we should log as replaced.
+
+                # incase we need the hex value
+                # newline = ":".join("{:02x}".format(ord(c)) for c in line)
+
+                # dollar sign - not an ascii character
+                line = line.replace('\xA2', '\x20')
+
+                file.write(line + "\n")
+                logger.log('Unable to parse line ' +
+                           str(count) + '. Take care when editing ' +
+                           'this! Printed: ' + line)
